734ee1485865a0826ce4868ff6a5c7e1284e67a9
valeratrades <v79166789533@gmail.com>
2024-11-11 15:20:11 +0000

    refactor: nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
-let
-	pkgs = import (builtins.fetchTarball {
-		url = "https://github.com/NixOS/nixpkgs/archive/7a339d87931bba829f68e94621536cad9132971a.tar.gz";
-	}) {};
-	myPkg = pkgs.lean4;
-in
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ myPkg ];
+#let
+#	pkgs = import (builtins.fetchTarball {
+#		url = "https://github.com/NixOS/nixpkgs/archive/7a339d87931bba829f68e94621536cad9132971a.tar.gz"; # gets lean4 v4.4.0, as v4.3.0 was not uploaded
+#	}) {};
+#	myPkg = pkgs.lean4;
+#in
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShellNoCC {
+  shellHook = ''
+    elan run 4.3.0 lake exe cache get && elan run 4.3.0 lake build Library && elan run 4.3.0 lake build AutograderLib
+  '';
 }

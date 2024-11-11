408b913d876ecaca2b7dde14d153245d1c239dd1
valeratrades <v79166789533@gmail.com>
2024-11-11 12:56:56 +0000

    feat: 02.1 done -b 2 cancels don't work correctly, but it's not my fault

--- Math2001/02_Proofs_with_Structure/01_Intermediate_Steps.lean
+++ Math2001/02_Proofs_with_Structure/01_Intermediate_Steps.lean
@@ -28,7 +28,7 @@ example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 := by
   calc
     r = (r + r) / 2 := by ring
     _ ≤ (3 - s + (3 + s)) / 2 := by rel[h3,h4]
-     _ = 3 := by ring
+    _ = 3 := by ring
 
 example {t : ℝ} (h1 : t ^ 2 = 3 * t) (h2 : t ≥ 1) : t ≥ 2 := by
   have h3 :=
@@ -56,21 +56,52 @@ example {x y : ℤ} (hx : x + 3 ≤ 2) (hy : y + 2 * x ≥ 3) : y > 3 := by
     _ > 3 := by numbers
 
 example (a b : ℝ) (h1 : -b ≤ a) (h2 : a ≤ b) : a ^ 2 ≤ b ^ 2 := by
+  have hp: (b+a) >= 0 := by addarith[h1]
+  have hm: (b-a) >= 0 := by addarith[h2]
   calc
-    a^2 = a * a := by ring --?
-    _ <= b ^ 2 := by sorry
+    a^2 <= a^2 + (b+a)*(b-a) := by extra
+    _ = b ^ 2 := by ring
 
 example (a b : ℝ) (h : a ≤ b) : a ^ 3 ≤ b ^ 3 := by
-  sorry
+  have hm: (b-a) >= 0 := by addarith[h]
+  calc
+    a^3 <= a^3 + (b-a)*((b-a)^2 + 3*(b+a)^2)/4 := by extra
+    _ = b^3 := by ring
 
 /-! # Exercises -/
 
 
 example {x : ℚ} (h1 : x ^ 2 = 4) (h2 : 1 < x) : x = 2 := by
+  have hm :=
+  calc
+    x * x = x^2 := by ring
+    _ = 2 * 2 := by addarith[h1]
+  --cancel x at hm
+  --have hx: x = 2 ∨ x = -2 := by nlinarith[h1]
   sorry
 
 example {n : ℤ} (hn : n ^ 2 + 4 = 4 * n) : n = 2 := by
-  sorry
+  have hs :=
+  calc
+    (n-2)^2 = n^2 +4 -4*n := by ring
+    _ = 4*n -4*n := by rw[hn]
+    _ = 0 := by ring
+  have hz :=
+  calc
+    (n-2)
+    _ = 0 := by exact pow_eq_zero hs
+  addarith[hz]
 
 example (x y : ℚ) (h : x * y = 1) (h2 : x ≥ 1) : y ≤ 1 := by
-  sorry
+  have hx : x > 0 := by addarith[h2]
+  have hy := 
+  calc
+    y = 1/1 * y := by ring
+    _ = x/x * y := by field_simp
+    _ = (x*y) / x := by ring
+    _  = 1 / x := by rw[h]
+  calc
+    y = y := by ring
+    _ = 1/x := by rw[hy]
+    _ <= 1/1 := by rel[h2]
+    _ = 1 := by ring

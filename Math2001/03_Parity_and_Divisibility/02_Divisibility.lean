f2a6ee13b3d51bcf4a84739603bd7e1528180f63
valeratrades <v79166789533@gmail.com>
2024-11-26 18:33:07 +0000

    feat: solved 3.2

--- Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
+++ Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
@@ -12,7 +12,9 @@ example : (11 : ℕ) ∣ 88 := by
 
 
 example : (-2 : ℤ) ∣ 6 := by
-  sorry
+  dsimp [(. ∣ .)]
+  use -3
+  numbers
 
 example {a b : ℤ} (hab : a ∣ b) : a ∣ b ^ 2 + 2 * b := by
   obtain ⟨k, hk⟩ := hab
@@ -23,12 +25,23 @@ example {a b : ℤ} (hab : a ∣ b) : a ∣ b ^ 2 + 2 * b := by
 
 
 example {a b c : ℕ} (hab : a ∣ b) (hbc : b ^ 2 ∣ c) : a ^ 2 ∣ c := by
-  sorry
+  dsimp[(· ∣ ·)]
+  obtain ⟨kab, hab⟩ := hab
+  obtain ⟨kbc, hbc⟩ := hbc
+  use kab^2 * kbc
+  calc
+    c = b^2 * kbc := by rw[hbc]
+    _ = (a*kab)^2 * kbc := by rw[hab]
+    _ = a^2 * (kab^2 * kbc) := by ring
 
 example {x y z : ℕ} (h : x * y ∣ z) : x ∣ z := by
-  sorry
+  obtain ⟨kxy, hxy⟩ := h
+  use y * kxy
+  calc
+    z = x * y * kxy := by rw[hxy]
+    _ = x * (y * kxy) := by ring
 
-example : ¬(5 : ℤ) ∣ 12 := by
+example : ¬((5 : ℤ) ∣ 12) := by
   apply Int.not_dvd_of_exists_lt_and_lt
   use 2
   constructor
@@ -51,34 +64,78 @@ example {a b : ℕ} (hb : 0 < b) (hab : a ∣ b) : a ≤ b := by
 
 
 example {a b : ℕ} (hab : a ∣ b) (hb : 0 < b) : 0 < a := by
-  sorry
+  obtain ⟨kab, hab⟩ := hab
+  have h0ltak :=
+  calc
+    0 < b := hb
+    _ = a * kab := by rw[hab]
+  cancel kab at h0ltak
 
 /-! # Exercises -/
 
 
 example (t : ℤ) : t ∣ 0 := by
-  sorry
+  use 0
+  ring
 
 example : ¬(3 : ℤ) ∣ -10 := by
-  sorry
+  apply Int.not_dvd_of_exists_lt_and_lt
+  use -4
+  constructor
+  . numbers
+  . numbers
 
 example {x y : ℤ} (h : x ∣ y) : x ∣ 3 * y - 4 * y ^ 2 := by
-  sorry
+  obtain ⟨kxy, hxy⟩ := h
+  use 3*kxy - 4*x*kxy^2
+  calc
+    3*y - 4*y^2 = 3*(x * kxy) - 4*(x * kxy)^2 := by rw[hxy]
+    _ = x * (3*kxy - 4*x*kxy^2) := by ring
 
 example {m n : ℤ} (h : m ∣ n) : m ∣ 2 * n ^ 3 + n := by
-  sorry
+  obtain ⟨kmn, hmn⟩ := h
+  use 2*m^2*kmn^3 + kmn
+  calc
+    2*n^3 + n = 2*(m * kmn)^3 + (m * kmn) := by rw[hmn]
+    _ = m* (2*m^2*kmn^3 + kmn) := by ring
 
 example {a b : ℤ} (hab : a ∣ b) : a ∣ 2 * b ^ 3 - b ^ 2 + 3 * b := by
-  sorry
+  obtain ⟨kab, hab⟩ := hab
+  use 2*a^2*kab^3 - a*kab^2 + 3*kab
+  calc
+    2*b^3 - b^2 + 3*b = 2*(a*kab)^3 - (a*kab)^2 + 3*(a*kab) := by rw[hab]
+    _ = a * (2*a^2*kab^3 - a*kab^2 + 3*kab) := by ring
 
 example {k l m : ℤ} (h1 : k ∣ l) (h2 : l ^ 3 ∣ m) : k ^ 3 ∣ m := by
-  sorry
+  obtain ⟨kkl, hkl⟩ := h1
+  obtain ⟨kl3m, hl3m⟩ := h2
+  use kkl^3 * kl3m
+  calc
+    m = l^3 * kl3m := by rw[hl3m]
+    _ = (k * kkl)^3 * kl3m := by rw[hkl]
+    _ = k^3 * (kkl^3 * kl3m) := by ring
 
 example {p q r : ℤ} (hpq : p ^ 3 ∣ q) (hqr : q ^ 2 ∣ r) : p ^ 6 ∣ r := by
-  sorry
+  obtain ⟨kp3q, hp3q⟩ := hpq
+  obtain ⟨kq2r, hq2r⟩ := hqr
+  use kp3q^2 * kq2r
+  calc
+    r = q^2 * kq2r := by rw[hq2r]
+    _ = (p^3 * kp3q)^2 * kq2r := by rw[hp3q]
+    _ = p^6 * (kp3q^2 * kq2r) := by ring
 
 example : ∃ n : ℕ, 0 < n ∧ 9 ∣ 2 ^ n - 1 := by
-  sorry
+  use 6
+  constructor
+  . numbers
+  . use 7
+    ring
 
 example : ∃ a b : ℤ, 0 < b ∧ b < a ∧ a - b ∣ a + b := by
-  sorry
+  use 2, 1
+  constructor
+  . numbers
+  . constructor
+    . numbers
+    . use 3
+      ring

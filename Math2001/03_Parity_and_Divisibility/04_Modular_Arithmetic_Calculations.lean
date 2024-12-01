446f1b39c16083cc1bced4770db2949469bfb616
valeratrades <v79166789533@gmail.com>
2024-12-01 16:43:14 +0000

    feat: 03.4 done

--- Math2001/03_Parity_and_Divisibility/04_Modular_Arithmetic_Calculations.lean
+++ Math2001/03_Parity_and_Divisibility/04_Modular_Arithmetic_Calculations.lean
@@ -15,7 +15,7 @@ example {a b : ℤ} (ha : a ≡ 4 [ZMOD 5]) (hb : b ≡ 3 [ZMOD 5]) :
   calc
     a * b + b ^ 3 + 3 ≡ 4 * b + b ^ 3 + 3 [ZMOD 5] := by rel [ha]
     _ ≡ 4 * 3 + 3 ^ 3 + 3 [ZMOD 5] := by rel [hb]
-    _ = 2 + 5 * 8 := by numbers
+    _ = 2 + 5 * 8 := by numbers --? is this equivalent to a `use` statement?
     _ ≡ 2 [ZMOD 5] := by extra
 
 
@@ -44,25 +44,70 @@ example {x : ℤ} : x ^ 3 ≡ x [ZMOD 3] := by
 
 /-! # Exercises -/
 
-
-example {n : ℤ} (hn : n ≡ 1 [ZMOD 3]) : n ^ 3 + 7 * n ≡ 2 [ZMOD 3] :=
-  sorry
+example {n : ℤ} (hn : n ≡ 1 [ZMOD 3]) : n ^ 3 + 7 * n ≡ 2 [ZMOD 3] := by
+  calc
+    n^3 + 7*n ≡ 1^3 + 7*1 [ZMOD 3] := by rel[hn]
+    _ = 2 + 2*3 := by numbers -- I think we can do this because transitivity
+    _ ≡ 2 [ZMOD 3] := by extra
 
 example {a : ℤ} (ha : a ≡ 3 [ZMOD 4]) :
-    a ^ 3 + 4 * a ^ 2 + 2 ≡ 1 [ZMOD 4] :=
-  sorry
+    a ^ 3 + 4 * a ^ 2 + 2 ≡ 1 [ZMOD 4] := by
+  calc
+    a^3 + 4*a^2 + 2 ≡ 3^3 + 4*3^2 + 2 [ZMOD 4] := by rel[ha]
+    _ = 4*16 + 1 := by numbers
+    _ ≡ 1 [ZMOD 4] := by extra
 
-example (a b : ℤ) : (a + b) ^ 3 ≡ a ^ 3 + b ^ 3 [ZMOD 3] :=
-  sorry
+example (a b : ℤ) : (a + b) ^ 3 ≡ a ^ 3 + b ^ 3 [ZMOD 3] := by
+  dsimp [Int.ModEq] at *
+  calc
+    (a + b)^3 = (a^3 + b^3) + 3*(a^2*b + a*b^2) := by ring
+    _ ≡ a^3 + b^3 [ZMOD 3] := by extra
 
 example : ∃ a : ℤ, 4 * a ≡ 1 [ZMOD 7] := by
-  sorry
+  use 9
+  calc
+    4*9 = 1 + 7*5 := by ring
+    _ ≡ 1 [ZMOD 7] := by extra
 
 example : ∃ k : ℤ, 5 * k ≡ 6 [ZMOD 8] := by
-  sorry
+  use 6
+  calc
+    5*6 = 6 + 8*3 := by ring
+    _ ≡ 6 [ZMOD 8] := by extra
 
 example (n : ℤ) : 5 * n ^ 2 + 3 * n + 7 ≡ 1 [ZMOD 2] := by
-  sorry
+  mod_cases hn: n%2
+  . calc
+    5*n^2 + 3*n + 7 ≡ 5*0^2 + 3*0 + 7 [ZMOD 2] := by rel[hn]
+    _ = 1 + 2*3 := by numbers
+    _ ≡ 1 [ZMOD 2] := by extra
+  . calc
+    5*n^2 + 3*n + 7 ≡ 5*1^2 + 3*1 + 7 [ZMOD 2] := by rel[hn]
+    _ = 1 + 2*7 := by numbers
+    _ ≡ 1 [ZMOD 2] := by extra
 
 example {x : ℤ} : x ^ 5 ≡ x [ZMOD 5] := by
-  sorry
+  mod_cases hx: x%5
+  . calc
+    x^5 ≡ 0^5 [ZMOD 5] := by rel[hx]
+    _ = 0 := by numbers
+    _ ≡ x [ZMOD 5] := by rel[hx]
+  . calc
+    x^5 ≡ 1^5 [ZMOD 5] := by rel[hx]
+    _ = 1 := by numbers
+    _ ≡ x [ZMOD 5] := by rel[hx]
+  . calc
+    x^5 ≡ 2^5 [ZMOD 5] := by rel[hx]
+    _ = 2 + 5*6 := by numbers
+    _ ≡ 2 [ZMOD 5] := by extra
+    _ ≡ x [ZMOD 5] := by rel[hx]
+  . calc
+    x^5 ≡ 3^5 [ZMOD 5] := by rel[hx]
+    _ = 3 + 5*48 := by numbers
+    _ ≡ 3 [ZMOD 5] := by extra
+    _ ≡ x [ZMOD 5] := by rel[hx]
+  . calc
+    x^5 ≡ 4^5 [ZMOD 5] := by rel[hx]
+    _ = 4 + 5*204 := by numbers
+    _ ≡ 4 [ZMOD 5] := by extra
+    _ ≡ x [ZMOD 5] := by rel[hx]

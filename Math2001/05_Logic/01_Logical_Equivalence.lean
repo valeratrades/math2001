95efde54236803c3bdff8ea676140d7c0985707f
valeratrades <v79166789533@gmail.com>
2024-12-08 19:32:54 +0000

    feat: 5.2 done

--- Math2001/05_Logic/01_Logical_Equivalence.lean
+++ Math2001/05_Logic/01_Logical_Equivalence.lean
@@ -43,7 +43,18 @@ example (P Q R : Prop) : (P ∧ (Q ∨ R)) ↔ ((P ∧ Q) ∨ (P ∧ R)) := by
       constructor
       · apply h1
       · apply h2
-  · sorry
+  · intro H
+    match H with
+    | Or.inl hpaq =>
+      constructor
+      . exact hpaq.1
+      . left
+        exact hpaq.2
+    | Or.inr hpar =>
+      constructor
+      . exact hpar.1
+      . right
+        exact hpar.2
 
 #truth_table P ∧ (Q ∨ R)
 #truth_table (P ∧ Q) ∨ (P ∧ R)
@@ -68,9 +79,10 @@ example {P : α → β → Prop} (h : ∃ x : α, ∀ y : β, P x y) :
 example (P : α → Prop) : ¬ (∃ x, P x) ↔ ∀ x, ¬ P x := by
   constructor
   · intro h a ha
-    have : ∃ x, P x
-    · use a
+    have : ∃ x, P x := by {
+      use a
       apply ha
+    }
     contradiction
   · intro h h'
     obtain ⟨x, hx⟩ := h'
@@ -81,43 +93,150 @@ example (P : α → Prop) : ¬ (∃ x, P x) ↔ ∀ x, ¬ P x := by
 
 
 example {P Q : Prop} (h : P ∧ Q) : P ∨ Q := by
-  sorry
+  right
+  exact h.2
 
 example {P Q R : Prop} (h1 : P → Q) (h2 : P → R) (h3 : P) : Q ∧ R := by
-  sorry
+  constructor
+  . exact (h1 h3)
+  . exact (h2 h3)
 
 example (P : Prop) : ¬(P ∧ ¬ P) := by
-  sorry
+  intro ⟨hp, hnp⟩
+  contradiction
 
 example {P Q : Prop} (h1 : P ↔ ¬ Q) (h2 : Q) : ¬ P := by
-  sorry
+  intro h
+  have:= h1.1 h
+  contradiction
 
 example {P Q : Prop} (h1 : P ∨ Q) (h2 : Q → P) : P := by
-  sorry
+  cases h1 with
+  | inl hp =>
+    exact hp
+  | inr hq =>
+    exact h2 hq
 
 example {P Q R : Prop} (h : P ↔ Q) : (P ∧ R) ↔ (Q ∧ R) := by
-  sorry
+  constructor
+  . intro hpar
+    constructor
+    . exact h.1 hpar.1
+    . exact hpar.2
+  . intro hqar
+    constructor
+    . exact h.2 hqar.1
+    . exact hqar.2
 
 example (P : Prop) : (P ∧ P) ↔ P := by
-  sorry
+  constructor
+  . intro hpap
+    exact hpap.1
+  . intro hp
+    constructor <;> exact hp
 
 example (P Q : Prop) : (P ∨ Q) ↔ (Q ∨ P) := by
-  sorry
+  constructor
+  . intro hpoq
+    cases hpoq with
+    | inl hp =>
+      right; exact hp
+    | inr hq =>
+      left; exact hq
+  . intro hqop
+    cases hqop with
+    | inl hq =>
+      right; exact hq
+    | inr hp =>
+      left; exact hp
 
 example (P Q : Prop) : ¬(P ∨ Q) ↔ (¬P ∧ ¬Q) := by
-  sorry
+  constructor
+  . intro h
+    -- both work:
+    -- # method 1
+    --have : ¬P ∧ ¬Q := ⟨fun p => h (Or.inl p), fun q => h (Or.inr q)⟩
+    --exact this
+    -- # method 2
+    constructor
+    . intro h
+      have: P ∨ Q := by {left; exact h}
+      contradiction
+    . intro h
+      have: P ∨ Q := by {right; exact h}
+      contradiction
+  . intro h
+    intro hpaq
+    cases hpaq with
+    | inl hp =>
+      have:=h.1
+      contradiction
+    | inr hq =>
+      have:=h.2
+      contradiction
 
 example {P Q : α → Prop} (h1 : ∀ x, P x → Q x) (h2 : ∀ x, P x) : ∀ x, Q x := by
-  sorry
+  intro x
+  have hp := h2 x
+  have hptq := h1 x
+  exact hptq hp
 
 example {P Q : α → Prop} (h : ∀ x, P x ↔ Q x) : (∃ x, P x) ↔ (∃ x, Q x) := by
-  sorry
+  constructor
+  . 
+    intro ⟨x, hx⟩
+    use x
+    exact (h x).1 hx
+  . 
+    intro ⟨x, hx⟩
+    use x
+    exact (h x).2 hx
 
 example (P : α → β → Prop) : (∃ x y, P x y) ↔ ∃ y x, P x y := by
-  sorry
+  constructor
+  . intro H
+    obtain ⟨x, y, hxy⟩ := H
+    use y, x
+    exact hxy
+  . intro H
+    obtain ⟨y, x, hxy⟩ := H
+    use x, y
+    exact hxy
 
 example (P : α → β → Prop) : (∀ x y, P x y) ↔ ∀ y x, P x y := by
-  sorry
+  constructor
+  . intro H
+    intro x y
+    exact H y x
+  . intro H
+    intro y x
+    exact H x y
 
 example (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) := by
-  sorry
+  constructor
+  . intro H
+    obtain ⟨⟨x, hx⟩, hq⟩ := H
+    use x
+    exact ⟨hx, hq⟩
+  . 
+    intro H
+    obtain ⟨x, hx, hq⟩ := H
+    constructor
+    . use x; exact hx
+    . exact hq
+
+-- same but more functional
+example (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) := by
+  constructor
+  . intro H
+    obtain ⟨⟨x, hx⟩, hq⟩ := H
+    exact ⟨x, ⟨hx, hq⟩⟩
+  . 
+    intro H
+    obtain ⟨x, hx, hq⟩ := H
+    exact ⟨⟨x, hx⟩, hq⟩
+
+-- same purely functional
+example (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) :=
+  ⟨fun ⟨⟨x, hx⟩, hq⟩ => ⟨x, ⟨hx, hq⟩⟩,
+   fun ⟨x, ⟨hx, hq⟩⟩ => ⟨⟨x, hx⟩, hq⟩⟩
